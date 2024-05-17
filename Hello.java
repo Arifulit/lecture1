2c57869a92ed05186ff083216c3ed9306fbbe955
Arifuliit <ariful.iit@gmail.com>
2024-05-17 07:59:49 -0800

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Vs code is Awesome...");
+
+        greeting("Ariful islam")
     }
+    public static greeting(String name){
+        System.out.println("Welcom " +name);
+    }
+   
 }
